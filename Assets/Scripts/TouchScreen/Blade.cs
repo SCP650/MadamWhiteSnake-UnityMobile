d5e0bd4279647a3f0ff5b9dca19afb93866ab5f8
Molly Huang <48228352+MollyAredtana@users.noreply.github.com>
2020-09-26 15:22:11 -0700

    Blade Cutting/Cut Trail

---
+++ Assets/Scripts/TouchScreen/Blade.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour {
+
+	public GameObject bladeTrailPrefab;
+	public float minCuttingVelocity = .001f;
+
+	bool isCutting = false;
+
+	Vector2 previousPosition;
+
+	GameObject currentBladeTrail;
+
+	Rigidbody2D rb;
+	Camera cam;
+	CircleCollider2D circleCollider;
+
+	void Start ()
+	{
+		cam = Camera.main;
+		rb = GetComponent<Rigidbody2D>();
+		circleCollider = GetComponent<CircleCollider2D>();
+	}
+
+	void StartCutting ()
+	{
+		isCutting = true;
+		currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+		previousPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+		circleCollider.enabled = false;
+	}
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Touch touch = Input.GetTouch(0);
+            // Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+        if(Input.touchCount > 0)
+        {
+           
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0f;
+            transform.position = touchPosition; 
+            StartCutting();
+        }
+
+    }
+
+}
