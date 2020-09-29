17693800f223212c5d885f19bc876f21bdc65fa1
Sebastian Yang <yukaiy@andrew.cmu.edu>
2020-09-29 16:04:15 -0400

    Seperate Camera

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform PlayerLocation;
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(PlayerLocation.position.x, transform.position.y, transform.position.z);
+    }
+}
