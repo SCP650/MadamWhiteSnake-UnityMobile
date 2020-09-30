03f18a4b229d95760dd921274c9431a6ecb37bfd
rich <qiyu1257@gamil.com>
2020-09-29 20:17:47 -0400

    adhealth

---
+++ Assets/Scripts/Skills/Heart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    [SerializeField] int AddHealthToPlayer = 30;
+   
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player");
+        Managers.Player.ChangeHealth(AddHealthToPlayer);
+        Destroy(gameObject);
+    }
+
+}
+
+
+
+
