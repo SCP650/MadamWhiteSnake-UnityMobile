a69cd9467d14df48f7e5b1f07760a1a0f2c95ef7
rich <qiyu1257@gamil.com>
2020-10-02 19:53:17 -0400

    boost

--- Assets/Scripts/Characters/PlayerViewHoriMove.cs
+++ Assets/Scripts/Characters/PlayerViewHoriMove.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class PlayerViewHoriMove : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
-    private Collider2D other;
-    void Start()
+    
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        StartCoroutine(Boostspeed());
+        StartCoroutine(boost());
         
-        IEnumerator Boostspeed()
+        IEnumerator boost()
         {
             if (other.gameObject.tag == "Boost");
             { 
             speed = 15.0f;
-                yield return new WaitForSeconds(3.0f);
+                yield return new WaitForSeconds(1.0f);
+            speed = 5.0f;
             }
         }
     }
-
     void Update()
     {
         //HorizontalInput = Input.GetAxis("Horizontal");
---
+++ Assets/Scripts/Items/Invincible.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invincible : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player");
+        Destroy(gameObject); 
+        //  用哪个mltp map 什么应该可以 限制health在一个范围  
+    }
+}
