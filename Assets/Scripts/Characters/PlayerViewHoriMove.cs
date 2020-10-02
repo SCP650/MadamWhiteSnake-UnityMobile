698065d0079377be8b75f8d4c9475b2bc884fbee
rich <qiyu1257@gamil.com>
2020-10-02 18:43:14 -0400

    boost

--- Assets/Scripts/Characters/PlayerViewHoriMove.cs
+++ Assets/Scripts/Characters/PlayerViewHoriMove.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class PlayerViewHoriMove : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
-   
+    private Collider2D other;
+    void Start()
+    {
+        StartCoroutine(Boostspeed());
+        
+        IEnumerator Boostspeed()
+        {
+            if (other.gameObject.tag == "Boost");
+            { 
+            speed = 15.0f;
+                yield return new WaitForSeconds(3.0f);
+            }
+        }
+    }
 
     void Update()
     {
         //HorizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * speed);
-    }
+    }   
 }
