bca3e8eae44ac47f1ee0cd38dabd976145abbffe
rich <qiyu1257@gamil.com>
2020-10-03 19:48:24 -0400

    items update

--- Assets/Scripts/Characters/PlayerViewHoriMove.cs
+++ Assets/Scripts/Characters/PlayerViewHoriMove.cs
@@ -5,29 +5,48 @@ using UnityEngine;
 public class PlayerViewHoriMove : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
-    private Collider2D other;
+    [SerializeField] int AddHealthToPlayer = 30;
 
-    void Start()
+
+private void OnTriggerEnter2D(Collider2D other)
     {
-        StartCoroutine(boost());        
-        IEnumerator boost()
-        {
-            if (other.gameObject.tag == "Boost");
-            { 
-            speed = 15.0f;
-                yield return new WaitForSeconds(1.0f);
-            speed = 5.0f;
-            }
-        }   //  先entercollider 然后 start放在里面， 之后出来rntercollider，再写 IEnumerator 
-        StartCoroutine(invincible());
-            IEnumerator invincible()
-            {
-                if (other.gameObject.tag == "Invincible");
-            
-                yield return new WaitForSeconds(2.0f);
-            }            
+        if (other.gameObject.tag == "Boost") {
+            Destroy(other.gameObject);
+            StartCoroutine(Addspeed());             
+         }
+
+        if (other.gameObject.tag == "Invincible"){ 
+        Destroy(other.gameObject);
+        StartCoroutine(Power());
+         }
+
+        if (other.gameObject.tag == "Heart"){
+         Destroy(other.gameObject);
+         StartCoroutine(Addhealth());
+         }
     }
-    void Update()
+        IEnumerator Addspeed()
+        {           
+        speed = 15.0f;
+        yield return new WaitForSeconds(1.0f);
+        speed = 5.0f;    
+         }
+
+        IEnumerator Power()
+        {
+         gameObject.tag = "Untagged";
+         yield return new WaitForSeconds(3.0f);
+         gameObject.tag = "Player";
+         }
+        IEnumerator Addhealth()
+        {
+         Managers.Player.ChangeHealth(AddHealthToPlayer);        
+         yield return null;
+         }
+
+
+
+void Update()
     {
         //HorizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * speed);
