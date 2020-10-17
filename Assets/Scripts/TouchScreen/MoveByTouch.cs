46c477a172c1cb1895b51f4701db6632c1ca4cb8
Molly Huang <48228352+MollyAredtana@users.noreply.github.com>
2020-10-17 11:30:21 -0700

    ImprovePlayer

--- Assets/Scripts/TouchScreen/MoveByTouch.cs
+++ Assets/Scripts/TouchScreen/MoveByTouch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MoveByTouch : MonoBehaviour
 {
@@ -33,7 +34,17 @@ public class MoveByTouch : MonoBehaviour
     // Screen Split
     private float width;
     private float MidWidth;
+    private float MidHeight;
     private CollectPoints dantianController;
+
+    public LayerMask groundLayer;
+
+    private float StartPosY;
+    private int count = 0;
+
+    private float StartTime;
+    private float CurTime;
+   
 
     void Start()
     {
@@ -42,18 +53,63 @@ public class MoveByTouch : MonoBehaviour
         canJump = true;
         width = Screen.width;
         MidWidth = width / 2;
+        MidHeight = Screen.height / 2;
         _animator = GetComponent<Animator>();
         dantianController = GetComponent<CollectPoints>();
         shield.SetActive(false);
+        //StartPosY =  0.1947699f;
+    }
+
+    bool IsGrounded() 
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 5.0f;
+        
+        // RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+
+        Debug.DrawRay(position, direction, Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        // if(hit)
+        // {
+        //     Debug.Log("HIT");
+        // }
+        // else
+        // {
+        //     Debug.Log("Not on ground");
+        // }
+		// //RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        
+
+        //RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if (hit) {
+            //Debug.Log("Touched object " + hit.transform.gameObject.name + " layer is " + hit.transform.gameObject.layer);
+            return true;
+        }
+        else
+        {
+            //Debug.Log("Nottt  on Ground ");
+        }
+        
+        return false;
     }
     void Update()
     {
 
-       
+        
+        
+        if(!IsGrounded())
+        {
+            //Debug.Log("Not on ground");
+        }
+        else{
+            //Debug.Log("On ground");
+        }
         if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             Touch touch = Input.GetTouch(0);
             Vector2 touchPos = touch.position;
+            StartTime = Time.time;
             
 
             if(touchPos.x < MidWidth)
@@ -74,12 +130,70 @@ public class MoveByTouch : MonoBehaviour
             else if(touchPos.x > MidWidth)
             {
                 //attack
-              
-                if (canJump)
-                {
-                    Jump();
-                }
+                // int yMovement = (int) Input.GetAxisRaw("Vertical");
+                // if (yMovement == 1) {
+                    // Jump();
+                // }
+                // if (canJump)
+                // {
+                //     Jump();
+                // }
                 
+                
+                //float CurY = rb.position.y;
+
+                // Debug.Log("In");
+                // Debug.Log("Pos now " + rb.position.y + " start pos " + StartPosY);
+                
+                // if(touch.phase == TouchPhase.Began)
+                // {
+                //     StartTime = Time.time;                    
+                // }
+                
+
+
+                // Debug.Log("start time is " + StartTime + "  cur time is " + CurTime);
+
+                if(touch.position.y < MidHeight)
+                {
+                    if(IsGrounded() && count < 2)// do first jump
+                    {
+                        count++;
+                        
+                        Jump();
+                        
+                    }
+                    else if(count < 2) // not on ground
+                    {
+                        Debug.Log("Second Jump");
+                        count++;
+                        // Debug.Log("Pppppos now " + rb.position.y);
+                        Jump();
+                        // Debug.Log("Pos now " + rb.position.y);
+                        
+                    }
+                     if(IsGrounded() && count >= 2)
+                    {
+                        //Debug.Log("INNNNN");
+                        // Debug.Log("Pos now " + rb.position.y);
+                        count = 0;
+                    }
+                }
+                else
+                {
+                    //Debug.Log("time to fly");
+                    // fly
+                    fly();
+
+                }
+
+        
+
+               
+                
+                
+        
+               
 
             } 
             
@@ -102,10 +216,38 @@ public class MoveByTouch : MonoBehaviour
             }
 
         }
+        
+    }
+
+    private void checktime()
+    {
+        CurTime = Time.time;
+    }
+
+    private void fly()
+    {
+        Debug.Log("fly");
+        
+        rb.velocity = new Vector2(rb.velocity.x * 2.5f, jumpSpeed * 2.0f);
+        rb.gravityScale = (float)(0.5);
+        int count  = 0;
+        while(count < 15)
+        {
+            rb.gravityScale += (float)(0.1);
+            count++;
+        }
+        rb.gravityScale = 2.0f;
+        
+        
+        // rb.gravityScale = 2.0f;
+        
     }
 
     private void Jump()
     {
+        //checktime();
+        
+        //Debug.Log("start time is " + StartTime + "  cur time is " + CurTime);
        _animator.SetBool("Jumping", true);
 
         rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
@@ -114,6 +256,18 @@ public class MoveByTouch : MonoBehaviour
         canJump = false;
         Managers.Audio.PlaySound(jumpSound);
         //shouldJump = true;
+    }
+
+    private void CheckID()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            // Check if finger is over a UI element
+            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            {
+                Debug.Log("Touched the UI");
+            }
+        }
     }
 
     //private void FixedUpdate()
