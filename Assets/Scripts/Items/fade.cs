2d39f38332a97ccb3860ba0adcceac6738c983fe
rich <qiyu1257@gamil.com>
2020-10-16 22:54:44 -0400

    RICH

---
+++ Assets/Scripts/Items/fade.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fade : MonoBehaviour
+{ 
+    public Animator anim;  
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            anim.Play("level2 虚化");
+        }            
+    }
+}
