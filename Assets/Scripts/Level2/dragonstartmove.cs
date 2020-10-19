0285979557dc59a4e0af6046b7717f05d523d38d
rich <qiyu1257@gamil.com>
2020-10-19 19:45:26 -0400

    rich

---
+++ Assets/Scripts/Level2/dragonstartmove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragonstartmove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag=="Player")
+        {
+            Debug.Log("startmoveing");
+            BroadcastMessage("dragonmoving");
+        }
+    }
+
+
+
+}
