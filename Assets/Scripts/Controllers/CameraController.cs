f537c3c584be0e705b6e4b0092a60baf46e26e49
rich <qiyu1257@gamil.com>
2020-10-23 23:53:44 -0400

    rich

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform PlayerLocation;
- 
+    public bool isMoving;
 
     // Update is called once per frame
+
+    void Awake()
+    {
+        Messenger.AddListener("cameramove", Move);
+    }
     void Update()
     {
-
         transform.position = new Vector3(PlayerLocation.position.x, transform.position.y, transform.position.z);
+
+        if (isMoving)
+        {
+            this.transform.position = new Vector3(PlayerLocation.position.x, -155.0f, transform.position.z);
+        }
+    }
+    void Move()
+    {
+        isMoving = true;
     }
 }
