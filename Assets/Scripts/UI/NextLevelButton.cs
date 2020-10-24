887906e96aeeeabd87bfc2fce19333dab83bd87c
Sebastian Yang <yukaiy@andrew.cmu.edu>
2020-10-23 22:01:56 -0400

    transition working

---
+++ Assets/Scripts/UI/NextLevelButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelButton : MonoBehaviour
+{
+    public void GoToNextLevel()
+    {
+        Time.timeScale = 1;
+        Managers.mission.GoToNextLevel();
+    }
+}
