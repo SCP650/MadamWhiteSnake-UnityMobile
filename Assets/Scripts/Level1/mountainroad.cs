a0dceda78dc545864d0512f7ab33740bbada8a83
rich <qiyu1257@gamil.com>
2020-10-25 19:41:14 -0400

    rich

---
+++ Assets/Scripts/Level1/mountainroad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mountainroad : MonoBehaviour
+{
+    public Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag =="Player")
+        {
+            anim.Play("Level1 mountain road");
+        }
+    }
+
+}
