7083cf21082a556912a239f602733f48fdc88d0d
Sebastian Yang <yukaiy@andrew.cmu.edu>
2020-10-25 23:11:56 -0400

    Tutorial added

---
+++ Assets/Scripts/UI/StartEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartEnemy : MonoBehaviour
+{
+    [SerializeField] GameObject EnemySpawer;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            EnemySpawer.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TutorialHint.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialHint : MonoBehaviour
+{
+    [SerializeField] Text hintText;
+    [SerializeField] string textContent;
+    [SerializeField] bool showTurnOff = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (showTurnOff)
+            {
+                StartCoroutine(ChangeHintText());
+            }
+            else
+            {
+                hintText.text = textContent;
+            }
+
+        }
+    }
+ 
+
+    private IEnumerator ChangeHintText()
+    {
+        yield return new WaitForSeconds(3);
+        hintText.text = textContent;
+   
+        hintText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(7);
+        hintText.text = "";
+    }
+
+}
