8b1c41b8758a37a812e56e71dddedafab40c4807
rich <qiyu1257@gamil.com>
2020-10-27 21:22:35 -0400

    rich

---
+++ Assets/Scripts/Level3/Level3camera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3camera : MonoBehaviour
+{
+    [SerializeField] Transform PlayerLocation;
+    public bool isMoving;
+
+    // Update is called once per frame
+
+    void Awake()
+    {
+        Messenger.AddListener("cameramove", Move);
+    }
+    void Update()
+    {
+        float y = transform.position.y;
+        if (isMoving && y > -155.0f)
+        {
+            y -= 5 * Time.deltaTime;
+        }
+        transform.position = new Vector3(PlayerLocation.position.x, y, transform.position.z);
+
+
+    }
+    void Move()
+    {
+        isMoving = true;
+        //this.transform.position = new Vector3(PlayerLocation.position.x, -155.0f, transform.position.z);
+
+    }
+}
