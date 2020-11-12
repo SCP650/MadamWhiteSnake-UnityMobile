62a9912a4dab3f9fa29b4f9f9a7baed5c18d4a6a
rich <qiyu1257@gamil.com>
2020-11-11 19:28:25 -0500

    1

---
+++ Assets/Scripts/Level2/CameraControllerLevel2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerLevel2 : MonoBehaviour
+{
+    [SerializeField] Transform PlayerLocation;
+    [SerializeField] Vector2 cameraspeed;
+    public bool isMoving;
+    private Transform cameraTransform;
+    private Vector3 lastPlayerPosition;
+
+    // Update is called once per frame
+
+
+    private void Start()
+    {
+        cameraTransform = PlayerLocation.transform;
+        lastPlayerPosition = cameraTransform.position;
+    }
+    private void LateUpdate()
+    {
+        Vector3 delatMovment = cameraTransform.position - lastPlayerPosition;
+        transform.position += new Vector3(delatMovment.x * cameraspeed.x, delatMovment.y * cameraspeed.y, 0);
+    }
+    void Update()
+    {
+        transform.position = new Vector3(PlayerLocation.position.x, PlayerLocation.position.y + 6.8f, transform.position.z);
+    }
+
+}
