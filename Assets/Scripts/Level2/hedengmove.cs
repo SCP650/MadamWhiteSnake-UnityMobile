fd7c797812806ad0c1e2991502773c7c44d6c7b8
rich <qiyu1257@gamil.com>
2020-11-15 23:31:33 -0500

    1

--- Assets/Scripts/Level2/hedengmove.cs
+++ Assets/Scripts/Level2/hedengmove.cs
@@ -7,16 +7,33 @@ public class hedengmove : MonoBehaviour
     [SerializeField] float Horizontalspeed;
     [SerializeField] float Frequency;
     [SerializeField] float Magnitude;
+    [SerializeField] float Waittime;
+    public bool isMoving;
     public Rigidbody2D r1;
-
-
+   void Awake()
+    {
+        Messenger.AddListener("hedengmoving", move);
+    }
     private void Start()
     {
         r1 = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
-        r1.velocity = new Vector3(-Horizontalspeed, Mathf.Sin(Time.time*Frequency)* Magnitude, 0.0f);
+        if (isMoving)
+        {
+            StartCoroutine("hedengMove");
+        }
+    }
+    IEnumerator hedengMove()
+    {
+        yield return new WaitForSeconds(Waittime);
+        r1.velocity = new Vector3(-Horizontalspeed, Mathf.Sin(Time.time * Frequency) * Magnitude, 0.0f);
+    }
 
+    void move()
+    {
+        isMoving = true;
     }
 }
