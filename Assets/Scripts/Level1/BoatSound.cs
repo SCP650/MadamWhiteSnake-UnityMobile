3baa36d7763f90aba7ecc068a94e597edd76697b
Sebastian Yang <yukaiy@andrew.cmu.edu>
2020-11-17 08:39:47 -0500

    sound for boat

---
+++ Assets/Scripts/Level1/BoatSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] stepOnBoat;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            Managers.Audio.PlaySound(stepOnBoat[Random.Range(0, stepOnBoat.Length)]);
+        }
+    }
+
+
+}
