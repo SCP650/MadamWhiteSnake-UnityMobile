f769cc327fa289b5e2772655833575d4a164440a
Sebastian Yang <yukaiy@andrew.cmu.edu>
2020-11-19 08:40:15 -0500

    fix camera

--- Assets/Scripts/Level2/CameraControllerLevel2.cs
+++ Assets/Scripts/Level2/CameraControllerLevel2.cs
@@ -8,20 +8,24 @@ public class CameraControllerLevel2 : MonoBehaviour
     [SerializeField] Vector2 cameraspeed;
     public bool isMoving;
     [SerializeField] float multiplier;
+    private float initDIff;
     void Awake()
     {
         Messenger.AddListener("cameramove3", Move);
+        initDIff = PlayerLocation.transform.position.y - Camera.main.transform.position.y;
     }
     void Update()
     {
-        float Dif= PlayerLocation.transform.position.y - Camera.main.transform.position.y;                        
-        transform.position = new Vector3 (PlayerLocation.position.x, PlayerLocation.position.y * (1- Dif * multiplier) + 6.8f , transform.position.z);       
+        float Dif= PlayerLocation.transform.position.y - Camera.main.transform.position.y - initDIff;                        
+        transform.position = new Vector3 (PlayerLocation.position.x, transform.position.y + (Dif * multiplier), transform.position.z);       
         float y = transform.position.y;
         if (isMoving && y > -119.82f)
-        {           
-            y -=2 * Time.deltaTime;                                   
+        {
+            Debug.Log("Lower camera");
+            y -=2 * Time.deltaTime;
+            transform.position = new Vector3(PlayerLocation.position.x, y, transform.position.z);
         }
-        transform.position = new Vector3(PlayerLocation.position.x, y, transform.position.z);
+        
     }
     void Move()
     {
