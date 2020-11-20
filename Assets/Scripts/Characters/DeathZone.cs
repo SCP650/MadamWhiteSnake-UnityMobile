245ad392fa8e6032ddd801596e52419bc34c4545
Sebastian Yang <yukaiy@andrew.cmu.edu>
2020-11-20 13:59:03 -0500

    fix flying when die

--- Assets/Scripts/Characters/DeathZone.cs
+++ Assets/Scripts/Characters/DeathZone.cs
@@ -11,6 +11,7 @@ public class DeathZone : MonoBehaviour
         if (gb.tag == "Player")
         {
             Managers.Audio.PlaySound(waterSound);
+            gb.GetComponent<Animator>().SetBool("Jumping", true);
             gb.transform.position = new Vector3(gb.transform.position.x, gb.transform.position.y + 35,gb.transform.position.z);
             Managers.Player.ChangeHealth(-Managers.Player.health/3);
         }
