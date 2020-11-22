a9172a94a742e9dec5ffaa9baae61f1bd9ff2900
Molly Huang <48228352+MollyAredtana@users.noreply.github.com>
2020-11-22 12:20:32 -0800

    Without countdown

--- Assets/Scripts/TouchScreen/Blade.cs
+++ Assets/Scripts/TouchScreen/Blade.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Blade : MonoBehaviour {
 
+	[SerializeField] Text warningText;
+
 	public GameObject bladeTrailPrefab;
 	public float minCuttingVelocity = .001f;
-
+	private CollectPoints dantianController;
+	// private CountDown CountController;
 	
 
 	Vector2 previousPosition;
@@ -22,6 +27,8 @@ public class Blade : MonoBehaviour {
 		cam = Camera.main;
 		rb = GetComponent<Rigidbody2D>();
 		circleCollider = GetComponent<CircleCollider2D>();
+		dantianController = GetComponent<CollectPoints>();
+		// CountController = GetComponent<CountDown>();
 	}
 
 	void StartCutting ()
@@ -41,13 +48,36 @@ public class Blade : MonoBehaviour {
             // Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
         if(Input.touchCount > 0)
         {
-           
-            Touch touch = Input.GetTouch(0);
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            touchPosition.z = 0f;
-            transform.position = touchPosition; 
-            StartCutting();
+           	if(dantianController.CurDanTian() >= 1)
+			{
+				// CountController.StartCount();
+				Time.timeScale = 0.4f;
+				Cut();
+			}
+			else
+			{
+				StartCoroutine(ShowWarning("丹田不足，无法滑切"));
+			}
         }
+
+    }
+
+	public void Cut()
+	{
+		Touch touch = Input.GetTouch(0);
+		// these 3 lines
+		Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+		touchPosition.z = 0f;
+		transform.position = touchPosition;
+		StartCutting();
+	}
+
+	private IEnumerator ShowWarning(string text)
+    {
+        warningText.text = text;
+        warningText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3);
+        warningText.gameObject.SetActive(false);
 
     }
 
