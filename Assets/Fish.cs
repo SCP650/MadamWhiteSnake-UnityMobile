03674355fa67f09f6ef1a3247a931d8bc2aa580c
rich <qiyu1257@gamil.com>
2020-11-30 18:41:23 -0500

    update

---
+++ Assets/Fish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            anim.Play("鲤鱼");
+        }
+    }
+}
