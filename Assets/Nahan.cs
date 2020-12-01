2cf2294ad39912e25006f7f940e30dd5a21658ed
rich <qiyu1257@gamil.com>
2020-12-01 16:51:08 -0500

    Add tower

---
+++ Assets/Nahan.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nahan : MonoBehaviour
+{
+    [SerializeField] private AudioClip playJiao;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Managers.Audio.PlaySound(playJiao);
+        }
+    }
+}
