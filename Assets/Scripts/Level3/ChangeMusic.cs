2ab4c56804fbb96aaef9727854e0952f76add6dd
Sebastian Yang <yukaiy@andrew.cmu.edu>
2020-12-01 13:06:12 -0500

    level3 music added

---
+++ Assets/Scripts/Level3/ChangeMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMusic : MonoBehaviour
+{ 
+    [SerializeField] string Level3EndMusic;
+    private AudioClip audio;
+
+    private void Start()
+    {
+        audio = Resources.Load(Level3EndMusic) as AudioClip;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+          
+            Managers.Audio.PlayBGM(audio);
+            //Managers.Audio.PlayLevelMusic(1);
+        }
+    }
+}
